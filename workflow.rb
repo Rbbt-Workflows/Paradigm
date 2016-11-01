df9046d662045232a11a407b9685a79994e33f36
Miguel Vazquez <miguel.vazquez@fdi.ucm.es>
2016-11-01 13:36:46 +0100

    nil

---
+++ workflow.rb
@@ -0,0 +1,18 @@
+require 'rbbt-util'
+require 'rbbt/workflow'
+
+Misc.add_libdir if __FILE__ == $0
+
+#require 'rbbt/sources/Paradigm'
+
+module Paradigm
+  extend Workflow
+
+
+end
+
+#require 'Paradigm/tasks/basic.rb'
+
+#require 'rbbt/knowledge_base/Paradigm'
+#require 'rbbt/entity/Paradigm'
+
