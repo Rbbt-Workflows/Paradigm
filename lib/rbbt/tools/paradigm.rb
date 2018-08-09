d4d0a2ef802aeeeff4ed3a4c3828c32d6ee71be8
Miguel Vazquez <miguel.vazquez.g@bsc.es>
2018-08-09 23:49:31 +0200

    Actually, remove boost

--- lib/rbbt/tools/paradigm.rb
+++ lib/rbbt/tools/paradigm.rb
@@ -14,7 +14,7 @@ module Paradigm
 
   def self.run(pathway, config, prefix = nil, verbose = false)
 
-    Rbbt.software.opt.boost.produce
+    #Rbbt.software.opt.boost.produce
     Rbbt.software.opt.libdai.produce
     cmd = Rbbt.software.opt.Paradigm.produce.paradigm.find
 
@@ -23,7 +23,7 @@ module Paradigm
     CMD.cmd(cmd, opts)
   end
 
-  Rbbt.claim Rbbt.software.opt.boost, :install, Rbbt.share.install.software.boost.find
+  #Rbbt.claim Rbbt.software.opt.boost, :install, Rbbt.share.install.software.boost.find
   Rbbt.claim Rbbt.software.opt.libdai, :install, Rbbt.share.install.software.libdai.find
   Rbbt.claim Rbbt.software.opt.Paradigm, :install, Rbbt.share.install.software.Paradigm.find
 
